dcafe38d1ec2ffcf106fa41b0ed14758ace3a6ee
Alastair Douglas <altreus@altre.us>
2018-05-22 20:49:25 +0100

    Check for spawning on each iteration

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
 var config = require('config');
 var roles = require('roles');
 
-console.log(Object.keys(config.targetCounts));
-
 // TODO: deal with multiple spawns
 var spawn = Game.spawns.BestSpawnYet;
 module.exports.loop = function() {
-    if (! spawn.spawning) {
-        for (let n in config.spawnPriority) {
-            let type = config.spawnPriority[n];
-            let count = config.targetCounts[type];
+    for (let n in config.spawnPriority) {
+        if (spawn.spawning) break;
+        let type = config.spawnPriority[n];
+        let count = config.targetCounts[type];
 
-            if (roles[type]) {
-                // FIXME: can we keep track of this?
-                let creeps = _.filter(Game.creeps, (creep) => creep.memory.role == type);
-                if (creeps.length < count) {
-                    console.log("Spawning " + type);
-                    var newCreep = roles[type].spawnAt(spawn)
-                }
+        if (roles[type]) {
+            // FIXME: can we keep track of this?
+            let creeps = _.filter(Game.creeps, (creep) => creep.memory.role == type);
+            if (creeps.length < count) {
+                console.log("Spawning " + type);
+                var newCreep = roles[type].spawnAt(spawn)
             }
         }
     }
