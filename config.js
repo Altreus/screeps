9314c7c95db1dfc375ed69d3def00c6830d5436c
Alastair Douglas <altreus@altre.us>
2018-05-22 22:51:21 +0100

    Remove boilerplate comment

--- config.js
+++ config.js
@@ -1,12 +1,3 @@
-/*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('config');
- * mod.thing == 'a thing'; // true
- */
-
 function howManyBuilders(spawn) {
     var constructions = spawn.room.find(FIND_CONSTRUCTION_SITES);
 
