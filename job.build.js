72d04ad474f5810eb58de4da693ed512d093a965
Alastair Douglas <altreus@altre.us>
2018-05-23 23:55:36 +0100

    Add build job

---
+++ job.build.js
@@ -0,0 +1,26 @@
+/* Builds a construction site.
+ * Puts the creep in harvest mode if it gets hungry.
+ *
+ * Returns:
+ * 0: Normal tick
+ * 1: Mode changed
+ * 2: Nothing to do
+ */
+module.exports = function(creep) {
+    let targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+    if(targets.length) {
+        var e = creep.build(targets[0]);
+        if(e == ERR_NOT_IN_RANGE) {
+            creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+        }
+        else if (e == ERR_NOT_ENOUGH_RESOURCES) {
+            creep.memory.mode = "harvest";
+            return 1;
+        }
+
+        return 0;
+    }
+    else {
+        return 2;
+    }
+}
