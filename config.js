e39e8551969dda63b6b73848345a1b9ea16b60ef
Alastair Douglas <altreus@altre.us>
2018-05-25 10:28:18 +0100

    At least 1builder if there's work to do

--- config.js
+++ config.js
@@ -4,7 +4,9 @@ function howManyBuilders(spawn) {
     var sum = (acc, c) => acc + parseInt(c.progressTotal, "10");
     var total = constructions.reduce(sum, 0);
 
-    return Math.trunc(total / 3000);
+    if (! total) return 0;
+
+    return Math.max(Math.trunc(total / 3000), 1);
 }
 
 module.exports = {
