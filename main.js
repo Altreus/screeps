7ef4e92dacc59945aeacb3b0802a749b22ab3e77
Alastair Douglas <altreus@altre.us>
2018-05-26 10:16:01 +0100

    Remove all "smart" code to see if it still works

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-var config = require('config');
 var jobs = require('jobs');
 
 function cleanupDeadCreeps() {
@@ -17,53 +16,14 @@ spawn.memory.serfs = spawn.memory.serfs || {};
 module.exports.loop = function() {
     cleanupDeadCreeps();
 
-    // Only spawning serfs for now
-    for (let type of config.spawnPriority) {
-        if (!! spawn.spawning) {
-            break;
-        }
+    let creeps = _.filter(Game.creeps, (creep) => true);
 
-        if (spawn.memory.tryAgainAt
-            && spawn.memory.tryAgainAt > spawn.energy) {
-            break;
-        }
-
-        if (spawn.memory.tryAgainAt) {
-            delete spawn.memory['tryAgainAt'];
-        }
-        let count = config.targetCounts[type](spawn);
-
-        console.log("Need " + count + " of " + type);
-
-        // FIXME: can we keep track of this?
-        let creeps = _.filter(Game.creeps, (creep) => creep.memory.role == type);
-        console.log("Have " + creeps.length);
-        if (creeps.length < count) {
-            console.log("Spawning " + type);
-            let c = jobs.serf.spawn(spawn, type);
-
-            // FIXME is this right?
-            if (c instanceof jobs.serf) {
-                console.log("New creep: " + c.creep.name + " is a " + c.creep.memory.role);
-                spawn.memory.serfs[c.creep.name] = c.creep.memory.role;
-            }
-            if (c == ERR_NOT_ENOUGH_ENERGY) {
-                console.log("Trying again at " + (spawn.energy + 50));
-                spawn.memory.tryAgainAt = spawn.energy + 50;
-            }
-
-            // Always break. If we call spawnCreep again, it overrides!
-            break;
-        }
+    if (creeps.length < 3) {
+        let c = jobs.serf.spawn(spawn, "charger");
     }
 
     for (let n in Game.creeps) {
         let c = Game.creeps[n];
-
-        if(!c) {
-            delete spawn.memory.serfs[n]
-            continue;
-        }
 
         new jobs[c.memory.job](c).tick();
     }
