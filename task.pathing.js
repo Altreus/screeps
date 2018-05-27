66c120ebf4acc8c1320e9674c630fe55e6e87853
Alastair Douglas <altreus@altre.us>
2018-05-28 00:17:33 +0100

    Prototype code to decide whether to retire or not

---
+++ task.pathing.js
@@ -0,0 +1,31 @@
+/* Pathing task. Reimplements moveTo in a way I can control.
+ *
+ * moveTo(creep, target, opts)
+ *
+ * Same as creep.moveTo(target, opts)
+ */
+
+module.exports = function(creep, target, opts) {
+    return creep.moveTo(target)
+
+    // TODO Cache path in some clever way
+    var path = PathFinder.search(creep.pos, target);
+    var moveSpeed = moveSpeed(creep);
+
+    var journeyTime = path.length * moveSpeed;
+
+    if (creep.ticksToLive < journeyTime) {
+        creep.mode = "retire";
+        return 2;
+    }
+    else {
+        creep.moveByPath(path);
+    }
+};
+
+/* Ticks per tile */
+function moveSpeed(creep) {
+    // TODO
+    // For now, just assume an average speed on plains
+    return 2
+}
